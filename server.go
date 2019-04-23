ef8b3d88c8062a35066d3b0cfed053af304a98ba
Ryan <rkerry1@gmail.com>
2019-04-23 23:46:03 +0100

    Build out the server to support redis and sessions service.

--- server.go
+++ server.go
@@ -1,17 +1,51 @@
 package matchmaker
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/gorilla/mux"
+	"github.com/pkg/errors"
+	predis "github.com/ryank90/matchmaker/pkg/redis"
+)
+
 const version string = "alpha-0.0.1"
 
 // Server is the http server instance.
 type Server struct {
+	srv      *http.Server
+	p        *redis.Pool
+	sessAddr string
 }
 
 // NewServer returns the HTTP server instance.
 func NewServer(hostAddr, redisAddr string, sessionAddr string) *Server {
-	return &Server{}
+	s := &Server{
+		p:        predis.NewPool(redisAddr),
+		sessAddr: sessionAddr,
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/healthz", predis.NewReadinessProbe(s.p))
+
+	s.srv = &http.Server{
+		Handler: r,
+		Addr:    hostAddr,
+	}
+
+	log.Printf("[log][server] connecting to server: %v on port: %v", version, hostAddr)
+	log.Printf("[log][server] connecting to redis: %v", redisAddr)
+	log.Printf("[log][server] connecting to sessions: %v", sessionAddr)
+
+	return s
 }
 
 // Start initialises the server.
 func (s *Server) Start() error {
-
+	err := predis.WaitForConnection(s.p)
+	if err != nil {
+		return errors.Wrap(err, "could not connect to redis")
+	}
+	return errors.Wrap(s.srv.ListenAndServe(), "error starting the server")
 }
--- session.go
+++ session.go
@@ -1,5 +1,7 @@
 package matchmaker
 
+import "github.com/garyburd/redigo/redis"
+
 const (
 	maxRetries = 20
 )
@@ -9,4 +11,12 @@ type Session struct {
 	ID   string `json:""`
 	Port int    `json:""`
 	IP   string `json:""`
+}
+
+func (s *Server) generateSessionForGame(c redis.Conn, g *Game) (*Game, error) {
+
+}
+
+func (s *Server) sessionIPAndPort(sess Session) (Session, error) {
+
 }
