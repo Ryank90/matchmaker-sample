863ec9877486f3f3b48fe13da2bc52db15a06c6b
Ryan <rkerry1@gmail.com>
2019-04-24 00:08:27 +0100

    Start a new server instance within main.go.

--- cmd/matchmaker/main.go
+++ cmd/matchmaker/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Ryank90/matchmaker"
+)
+
+func main() {
+	log.Print("[info][main] creating server...")
+	s := matchmaker.NewServer(":"+os.Getenv("PORT"), os.Getenv("REDIS_SERVICE"), os.Getenv("SESSION_SERVICE"))
+	if err := s.Start(); err != nil {
+		log.Fatalf("[error][main] %+v", err)
+	}
 }
